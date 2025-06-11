b833389bc63226a34e34982cdef83d994795c3ac
haily <49448330+melly8954@users.noreply.github.com>
2025-06-12 06:51:25 +0900

    feat: 모임 로켓 전송 시 참여자들 화면변경을 위한 sub, pub 추가

--- src/main/java/com/melly/timerocketserver/websocket/service/GroupRocketWebSocketService.java
+++ src/main/java/com/melly/timerocketserver/websocket/service/GroupRocketWebSocketService.java
@@ -4,6 +4,8 @@ import com.melly.timerocketserver.websocket.dto.response.ReadyStatusMsgResponse;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class GroupRocketWebSocketService {
     private final SimpMessagingTemplate messagingTemplate;
@@ -15,5 +17,13 @@ public class GroupRocketWebSocketService {
     public void publishReadyStatus(Long groupId, Long userId, int round, boolean isReady) {
         ReadyStatusMsgResponse message = new ReadyStatusMsgResponse(userId, round, isReady);
         messagingTemplate.convertAndSend("/topic/group/" + groupId + "/readyStatus", message);
+    }
+
+    public void publishRocketSent(Long groupId, Long userId) {
+        Map<String, Object> payload = Map.of(
+                "type", "rocketSent",
+                "senderId", userId
+        );
+        messagingTemplate.convertAndSend("/topic/group/" + groupId + "/send", payload);
     }
 }
