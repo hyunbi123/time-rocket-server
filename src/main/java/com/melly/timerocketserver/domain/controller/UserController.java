ccd14e5c2a460ac6c2f7c13243feff368ae9a1dd
haily <49448330+melly8954@users.noreply.github.com>
2025-06-25 19:31:12 +0900

    refactor: 닉네임 중복 체크 API RESTful하게 변경 및 유효성 검사 수정 (250625)

--- src/main/java/com/melly/timerocketserver/domain/controller/UserController.java
+++ src/main/java/com/melly/timerocketserver/domain/controller/UserController.java
@@ -38,9 +38,10 @@ public class UserController implements ResponseController {
         return makeResponseEntity(HttpStatus.CREATED, "회원가입 성공", null);
     }
 
-    @GetMapping("/users/duplicate-nickname/{nickname}")
-    public ResponseEntity<ResponseDto> duplicateNickname(@PathVariable @Pattern(regexp = "^[a-zA-Z0-9]{2,}$", message = "닉네임은 영어 또는 숫자로 구성된 2자 이상이어야 합니다.")
-                                                         String nickname) {
+    @GetMapping("/users/duplicate-nickname")
+    public ResponseEntity<ResponseDto> checkNickname(@RequestParam("nickname")
+                                                     @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,}$", message = "닉네임은 한글, 영어 또는 숫자로 구성된 2자 이상이어야 합니다.")
+                                                     String nickname) {
         userService.duplicateNickname(nickname);
         return makeResponseEntity(HttpStatus.OK, "중복 체크 완료", null);
     }
